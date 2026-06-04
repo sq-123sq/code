008918f165315dfc5465d77c10abdc76b9957726
sq-123sq <3642504965@qq.com>
2026-06-04 22:56:20 +0800

    6.4

--- game/io_file.h
+++ game/io_file.h
@@ -4,6 +4,6 @@
 int readclick(int* x, int* y);
 
 // 将棋盘内容写入文件供网页读取
-void writemap(char* board, int row, int col, int actual_cols);
+void writemap(char* board, int row, int col, int actual_cols,player* p, enemy* e);
 
 void writestatus(player* p, enemy* e, int game_over, int winner);
